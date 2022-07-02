2ee1355511af0d9a2905c4b4fc6776c0240f353b
seba nigu <seba80443@gmail.com>
2022-07-02 23:33:43 +0300

    add onDrag events

--- client/src/pages/Dashboard/Jira.js
+++ client/src/pages/Dashboard/Jira.js
@@ -1,7 +1,17 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 function Jira(props) {
   const [list, setList] = useState(props.data);
+  const [onDragItem, setOnDragItem] = useState(false);
+  const [currentItem, setCurrentItem] = useState({});
+
+  const handleDragEnd = () => {
+    console.log("test");
+    setOnDragItem(false);
+  };
+  const handleDragStart = () => {
+    setOnDragItem(true);
+  };
 
   return (
     <div className="flex-1 flex flex-col">
@@ -9,15 +19,24 @@ function Jira(props) {
         <div className="jira-title text-5xl font-bold">Jira clone</div>
       </div>
       <div className="p-8 flex justify-between flex-1">
-        {list?.map((grp) => (
-          <div>
+        {list?.map((grp, grpI) => (
+          <div key={grpI}>
             <div className="dnd-group bg-gray-100 w-72 p-4 min-h-[15rem] rounded">
               <div className="dnd-title text-gray-400 font-semibold">
                 {grp.title} <span>{grp.items.length}</span>
-                test
               </div>
               {grp.items.map((item) => (
-                <div draggable className="dnd-item bg-white p-5 m-2">
+                <div
+                  key={item.taskId}
+                  draggable
+                  onDragEnd={handleDragEnd}
+                  onDragStart={handleDragStart}
+                  className={
+                    !onDragItem
+                      ? "dnd-item bg-white p-5 m-2"
+                      : "dnd-item bg-blue-700 p-5 m-2"
+                  }
+                >
                   <div className="dnd-text font-roboto">{item.text}</div>
                 </div>
               ))}
