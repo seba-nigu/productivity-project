026db48883a4dcc8baf80ff9d95877c6fafdff53
seba nigu <seba80443@gmail.com>
2022-07-04 16:54:08 +0300

    add jira route to backend logic

--- server/routes/protectRoutes.js
+++ server/routes/protectRoutes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { protect } = require("../middlewares/authMiddleware");
+const { getTasks } = require("../controllers/taskController");
 
 router.get("/", protect);
+router.post("/jira", getTasks);
 
 module.exports = router;
